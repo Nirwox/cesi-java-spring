349a73a8e15cc67fa68c16715047bc09a05c8b1d
Nirwox <hugo.louvet@hotmail.fr>
2020-10-15 10:12:47 +0200

    Projet Fonctionnel

--- src/main/java/com/cesi/spring/controller/HelloWordController.java
+++ src/main/java/com/cesi/spring/controller/HelloWordController.java
@@ -1,15 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.cesi.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Hugo-Louvet
  */
 @RestController
+@RequestMapping("/rest/1")
 public class HelloWordController {
     @RequestMapping("/")
     public String index() {
